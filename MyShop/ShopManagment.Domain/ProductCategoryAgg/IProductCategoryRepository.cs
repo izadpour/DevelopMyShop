6aad0f3a8488b94a43f0714dc44a1e7b5a4f1b25
t <t@DESKTOP-U2QKNRE>
2021-08-19 04:10:42 +0430

    ProductCategory

---
+++ MyShop/ShopManagment.Domain/ProductCategoryAgg/IProductCategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShopManagement.Domain.ProductCategoryAgg
+{
+   public interface IProductCategoryRepository
+   {
+       void Create(ProductCategory entity);
+       ProductCategory Get(long id);
+       List<ProductCategory> GetAll();
+   }
+}
